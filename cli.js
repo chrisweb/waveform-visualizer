329b5faeeace6e39a7687af9627a5cf5f05e40cf
chrisweb <chris963@gmail.com>
2014-09-30 12:54:41 +0200

    minor fixes

--- cli.js
+++ cli.js
@@ -4,6 +4,18 @@ var queryObject = {};
 
 process.argv.forEach(function (value, index, array) {
     
+    var commandParametersLength = array.length - 2;
+    
+    if (commandParametersLength < 5 || commandParametersLength > 6) {
+        
+        var error = 'invalid parameters length, please call the cli with the following parameters: node cli SERVER_PATH TRACK_NAME TRACK_EXTENSION AMOUT_OF_PEAKS LOCAL_OR_REMOTE OPTIONAL_OUTPUT_FORMAT\n';
+        
+        process.stderr.write(error + "\n");
+
+        process.exit(1);
+        
+    } 
+    
     if (index === 2) {
         
         queryObject.serverDirectory = value;
@@ -12,25 +24,31 @@ process.argv.forEach(function (value, index, array) {
     
     if (index === 3) {
         
-        queryObject.trackId = value;
+        queryObject.trackId = parseInt(value);
         
     }
     
     if (index === 4) {
         
-        queryObject.trackFormat = value;
+        queryObject.trackFormat = value.toLowerCase();
         
     }
     
     if (index === 5) {
         
-        queryObject.peaksAmount = value;
+        queryObject.peaksAmount = parseInt(value);
         
     }
     
     if (index === 6) {
         
-        queryObject.service = value;
+        queryObject.service = value.toLowerCase();
+        
+    }
+    
+    if (index === 7) {
+        
+        queryObject.outputFormat = value.toLowerCase();
         
     }
     
@@ -40,23 +58,39 @@ var outputResponse = function outputResponseFunction(error, peaks) {
 
     if (error) {
 
-        process.stderr.write(error);
+        process.stderr.write(error + "\n");
 
         process.exit(1);
 
     } else {
 
-        var peaksString = '';
-        var i;
-        var peaksLength = peaks.length;
-        
-        for (i = 0; i < peaksLength; i++) {
+        var output = '';
+
+        if (queryObject.outputFormat === 'json') {
+
+            var outputData = {
+                "peaks": peaks
+            };
             
-            peaksString += peaks[i] + ',';
+            output = JSON.stringify(outputData);
+            
+        } else {
+            
+            var peaksString = '';
+            var i;
+            var peaksLength = peaks.length;
+
+            for (i = 0; i < peaksLength; i++) {
+
+                peaksString += peaks[i] + ',';
+
+            }
+            
+            output = peaksString.substring(0, peaksString.length - 1);
             
         }
-
-        process.stdout.write(peaksString.substring(0, peaksString.length - 1));
+        
+        process.stdout.write(output + "\n");
 
         process.exit(0);
 
