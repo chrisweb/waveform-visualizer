d8272374f45cf6c9e51fe4b98b582901056d3d72
chrisweb <chris963@gmail.com>
2023-09-09 03:48:19 +0200

    fix js scope bug for event listener

--- src/library/core.ts
+++ src/library/core.ts
@@ -126,13 +126,13 @@ export class Waveform {
 
     protected _addClickWaveListener(): void {
 
-        this._canvasElement.addEventListener('click', this._canvasElementClick);
+        this._canvasElement.addEventListener('click', (event: MouseEvent) => { this._canvasElementClick(event) });
 
     }
 
     protected _removeClickWaveListener(): void {
 
-        this._canvasElement.removeEventListener('click', this._canvasElementClick);
+        this._canvasElement.removeEventListener('click', (event: MouseEvent) => { this._canvasElementClick(event) });
 
     }
 
